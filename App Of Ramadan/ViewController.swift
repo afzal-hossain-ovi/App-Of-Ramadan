d6cecafec8ccbdc589506de31ce3bc2c7fcdf047
Md AfzaL Hossain <aafzal322@gmail.com>
2017-05-24 13:19:28 +0600

    Initial Commit

---
+++ App Of Ramadan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App Of Ramadan
+//
+//  Created by Md AfzaL Hossain on 5/24/17.
+//  Copyright © 2017 Md AfzaL Hossain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
